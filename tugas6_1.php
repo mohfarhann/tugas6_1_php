8c718221a6a77e760fb3ee7d8d5c7e7813d29bbc
mohfarhann <90471536+mohfarhann@users.noreply.github.com>
2022-05-19 14:10:40 +0700

    Add files via upload

---
+++ tugas6_1.php
@@ -0,0 +1,13 @@
+<?php
+
+  $x = 21;
+  $y = $x + 4;
+  $z = $y + 3;
+
+  echo "==========================";
+  echo "</br>Usia Habib = $x";
+  echo "</br>Usia Aji = $y";
+  echo "</br>Usia Bintan = $z";
+  echo "</br>==========================";
+
+ ?>
